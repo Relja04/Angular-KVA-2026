70d23495ee998e657ab1f1f5876545a87c1378dc
Relja <reljavukcevic@gmail.com>
2026-03-04 10:48:13 +0100

    Changed header h2 to a tags Added about us, contact and faq components Connected them with router link in header.html Added routes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        loadComponent: () => {
+            return import("./home/home").then(
+                m => m.Home
+            )
+        }
+    },
+    {
+        path:"about",
+        loadComponent:()=>{
+            return import("./about/about").then(
+                a=>a.About
+            )
+        }
+    },
+    {
+        path:"contact",
+        loadComponent:()=>{
+            return import("./contact/contact").then(
+                c=>c.Contact
+            )
+        }
+    },
+    {
+        path:"faq",
+        loadComponent:()=>{
+            return import("./faq/faq").then(
+                f=>f.Faq
+            )
+        }
+    }
+];
