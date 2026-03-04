dcc28bb94148c4129700aff1e447a04579df0dbe
Relja <reljavukcevic@gmail.com>
2026-03-04 12:50:51 +0100

    Added footer and filled the home page with toys Axios added

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import axios from 'axios'
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+    selector: 'app-home',
+    imports: [],
+    templateUrl: './home.html',
+    styleUrl: './home.css',
 })
 export class Home {
+    image: string="https://toy.pequla.com/"
+    toys=signal<any[]>([])
+    constructor(){
+        axios.get("https://toy.pequla.com/api/toy").then(
+            rsp=>this.toys.set(rsp.data)
+        )
+    }
+    selectedToy: any = null;
 
+openDetails(toy: any) {
+  this.selectedToy = toy;
+}
+
+closeDetails() {
+  this.selectedToy = null;
+}
 }
