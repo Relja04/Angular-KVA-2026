b1e74cc0acb4030171099960eeb6085a62f1c7cb
Relja <reljavukcevic@gmail.com>
2026-05-09 17:26:59 +0200

    Name search implemented

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,14 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { ToyModel } from '../models/toy.model';
 import { Utils } from '../utils';
 import { AuthService } from '../services/auth.service';
 import { RouterLink } from "@angular/router";
 import { ToyService } from '../services/toy.service';
 import { CartService } from '../services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-home',
-    imports: [RouterLink],
+    imports: [RouterLink, FormsModule],
     templateUrl: './home.html',
     styleUrl: './home.css',
 })
@@ -16,6 +17,7 @@ export class Home {
     service = AuthService
     image: string = "https://toy.pequla.com/"
     toys = signal<ToyModel[]>([])
+    searchTerm = signal<string>('');
     constructor(public utils: Utils,private cartService:CartService) {
         cartService.refreshUserCart()
         ToyService.getToys().then(
@@ -24,6 +26,12 @@ export class Home {
     }
     selectedToy: any = null;
 
+    filteredToys = computed(() => {
+        const term = this.searchTerm().toLowerCase();
+        return this.toys().filter(t => 
+            t.name.toLowerCase().includes(term)
+        );
+    });
     openDetails(toy: any) {
         this.selectedToy = toy;
     }
